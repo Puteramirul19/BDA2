f1478271395a868cd6a7703af3dc9ca662cf3779
Putera Amirul <puteraamirulhaqim19@gmail.com>
2025-09-04 11:00:16 +0800

    phase 5.0

---
+++ BDA/wwwroot/js/UMA/create.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    // Show create buttons
+    $("#CreateGroupBtn").show();
+
+    $("#btnCreate").on('click', function (e) {
+        if ($("#formCreateUMA").valid()) {
+            $("#Status").val("Submit");
+            $("#formCreateUMA").submit();
+        } else {
+            ShowNoty("error", "Please fill all required fields!");
+        }
+    });
+
+    $("#btnDraft").on('click', function (e) {
+        if ($("#formCreateUMA").valid()) {
+            $("#Status").val("Draft");
+            $("#formCreateUMA").submit();
+        } else {
+            ShowNoty("error", "Please fill all required fields!");
+        }
+    });
+});
+
+function ShowNoty(type, message) {
+    new Noty({
+        type: type,
+        layout: 'topRight',
+        text: message,
+        timeout: 3000
+    }).show();
+}
